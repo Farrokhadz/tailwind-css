efe0a1e80d0b09e6568d41b149eacebdb05b2b41
Farrokh <farrokhandarz84@gmail.com>
2026-08-15 01:20:44 +0330

    install react icon library

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import "./App.css";
-import React from "react";
-
+import { CiHeart } from "react-icons/ci";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <CiHeart className="text-3xl"></CiHeart>
+        <button
+          onClick={() => {}}
+          className="inline-block bg-fuchsia-700 px-7 py-3 rounded-full hover:bg-fuchsia-600"
+        >
+          {" "}
+          My Button
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
