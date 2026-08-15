c2c720a9247725d355ac6787d04ce0e5fb1b0acc
Farrokh <farrokhandarz84@gmail.com>
2026-08-15 16:07:25 +0330

    delete liked button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import "./App.css";
-import LikeButton from "./LikeButton.jsx";
 function App() {
-  return (
-    <>
-      <LikeButton></LikeButton>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
