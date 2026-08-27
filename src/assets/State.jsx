bc122432a033743332f684de78923f2b2914d3f6
Farrokh <farrokhandarz84@gmail.com>
2026-08-27 15:08:11 +0330

    add state.jsx (summrize text with toggle)

--- src/assets/State.jsx
+++ src/assets/State.jsx
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
 
 function State() {
-  const [isExpanded, setisExpanded] = useState(true);
-  const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sint
-  odio numquam, veritatis earum, impedit magni consequuntur architecto
-  officiis quia magnam alias optio nemo nisi. Cum quis commodi deleniti
-  debitis.`;
-  const handleOnclick = function (maxlegnth) {
-    text.slice(0, maxlegnth);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const text = `ipsum dolor sit amet consectetur adipisicing elit. Sapiente quaerat eius optio accusantium quisquam odit.
+  Minus animi quibusdam modi libero aliquam explicabo molestiae cum iusto. Ipsa nemo cum soluta dignissimos
+  exercitationem esse excepturi ea, iusto beatae voluptate quisquam consequatur labore placeat nam nostrum
+  laborum! Voluptates, saepe aut explicabo error minima exercitationem sunt quis enim nulla ipsam, dolore
+  eaque nostrum odio eos! Eum excepturi culpa dolorum. Ad dicta iste velit eveniet temporibus. Saepe voluptatem
+  aspernatur cumque praesentium quae doloribus maxime dolores aliquam exercitationem eaque, odit ea, ullam ex
+  assumenda. Doloremque fuga totam ratione deleniti error alias saepe. Nisi unde at perferendis eum tempora
+  fuga accusamus. Magni impedit quibusdam molestiae laboriosam! Blanditiis, officia incidunt ut minus quos`;
+
+  const maxLength = 200;
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
   };
+
+  const displayText =
+    isExpanded || text.length <= maxLength
+      ? text
+      : text.slice(0, maxLength) + "...";
 
   return (
     <>
-      <p>{text}</p>
-      <button onClick={handleOnclick}>more</button>
+      <p>{displayText}</p>
+      {text.length > maxLength && (
+        <button onClick={toggleExpand}>{isExpanded ? "less" : "more"}</button>
+      )}
     </>
   );
 }
